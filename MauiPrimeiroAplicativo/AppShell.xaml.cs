f89ee2557dac89c893cc6f1a69f5bd7e0b6d5932
Usuario <Usuario@CPD03-M11>
2024-02-19 15:53:40 -0300

    Adicionar arquivos de projeto.

---
+++ MauiPrimeiroAplicativo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPrimeiroAplicativo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiPrimeiroAplicativo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPrimeiroAplicativo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
